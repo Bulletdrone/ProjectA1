6af758ff0ca89794a8f5853e54c69296d94058ec
m.eind <36227686+4561351658456@users.noreply.github.com>
2019-04-18 10:06:07 +0200

    Added a testing menu 4 tracking script

--- Wonder Route/Assets/Scripts/Navigation/Tracking.cs
+++ Wonder Route/Assets/Scripts/Navigation/Tracking.cs
@@ -1,30 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Android;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Tracking : MonoBehaviour
 {
     public int maxDistance;
+    public Text _naviText;
+    public Text _statusText;
 
-    // Start is called before the first frame update
-    void Start()
+    public void Tracker()
     {
         StartCoroutine(StartGPSTracker());
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _statusText.text = "Currently tracking";
     }
 
     IEnumerator StartGPSTracker()
     {
-        yield return new WaitForSeconds(3);
+       _naviText.text = "Tracking...";
+
+       yield return new WaitForSeconds(3);
 
         if (!Input.location.isEnabledByUser)
+        {
+            _statusText.text = "Currently not tracking";
             yield break;
+        }
 
         Input.location.Start(maxDistance);
 
@@ -33,29 +35,29 @@ public class Tracking : MonoBehaviour
         while(Input.location.status == LocationServiceStatus.Initializing && waitUntilTimeOut > 0)
         {
             yield return new WaitForSeconds(1);
-            Debug.Log (Input.location.status);
+            _naviText.text = Input.location.status.ToString();
             waitUntilTimeOut--;
         }
 
         if (waitUntilTimeOut < 1)
         {
-            Debug.Log("Timed Out");
+            _naviText.text = "Timed Out";
             yield break;
         }
 
         if (Input.location.status == LocationServiceStatus.Failed)
         {
-            Debug.Log("Failed to launch locationservice");
+            _naviText.text = "Failed to launch locationservice";
             yield break;
         }
         if (Input.location.status == LocationServiceStatus.Running)
         {
-            Debug.Log(Input.location.status);
-            //Debug.Log("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+            //_naviText.text = Input.location.status.ToString();
+            _naviText.text = "Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp;
         }
         else
         {
-            Debug.Log("Nothing Happens");
+            _naviText.text = "Nothings happening";
         }
 
 
