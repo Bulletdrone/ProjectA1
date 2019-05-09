2dd6432bf85f07c90896c67e364b300154de8c9b
Bulletdrone <Samboots123@gmail.com>
2019-05-09 10:33:15 +0200

    Locations Script
    Locations Script to hold the GPS location.

---
+++ Wonder Route/Assets/Scripts/Locations/Locations.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Locations : MonoBehaviour
+{
+    public enum Location {SintLucasIngang, Microlab, PSVStadium, MCdonalds, End};
+    int[,] gpsData; // 
+
+    void Start()
+    {
+        gpsData = new int[(int)Location.End - 1, 4];
+    }
+
+    public int[] GetLocation(Location loc)
+    {
+        int[] data = new int[gpsData.GetLength(1)];
+        int locInt = (int)loc;
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            data[i] = gpsData[locInt, i];
+        }
+
+        return data;
+    }
+}
