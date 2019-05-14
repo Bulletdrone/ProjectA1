ee838176ae4e1e20ea21e92bd9223cc6d8a7c27d
Bulletdrone <Samboots123@gmail.com>
2019-05-14 09:54:15 +0200

    Locations script
    De locations script samen met de wat andere files zoals de json file voor cordinates.

--- Wonder Route/Assets/Scripts/Locations/Locations.cs
+++ Wonder Route/Assets/Scripts/Locations/Locations.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class Locations : MonoBehaviour
 {
@@ -9,19 +10,45 @@ public class Locations : MonoBehaviour
 
     void Start()
     {
-        gpsData = new int[(int)Location.End - 1, 4];
+        gpsData = new int[(int)Location.End - 1, 2];
+
+        SetLocation();
+    }
+
+    public void SetLocation()
+    {
+        for (int i = 0; i < gpsData.GetLength(0); i++)
+        {
+            gpsData[i, 0] = i + 1;
+            Debug.Log(gpsData[i, 0]);
+            gpsData[i, 1] = i + 2;
+            Debug.Log(gpsData[i, 1]);
+        }
     }
 
     public int[] GetLocation(Location loc)
     {
-        int[] data = new int[gpsData.GetLength(1)];
+        int[] intData = new int[gpsData.GetLength(1)];
         int locInt = (int)loc;
 
-        for (int i = 0; i < data.Length; i++)
+        for (int i = 0; i < intData.Length; i++)
         {
-            data[i] = gpsData[locInt, i];
+            intData[i] = gpsData[locInt, i];
         }
 
-        return data;
+        return intData;
+    }
+
+    [System.Serializable]
+    public class LocationInfo
+    {
+        public int[] cords;
+        //public string locName;
+    }
+
+    [System.Serializable]
+    public struct LocationCollection
+    {
+        public LocationInfo[] locationCollection;
     }
 }
