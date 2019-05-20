ee689d12263e23d9a849386115203d3495bc24de
Bulletdrone <Samboots123@gmail.com>
2019-05-20 11:46:49 +0200

    Camerarotation by gyro

---
+++ Wonder Route/Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localRotation = Input.gyro.attitude;
+    }
+}
