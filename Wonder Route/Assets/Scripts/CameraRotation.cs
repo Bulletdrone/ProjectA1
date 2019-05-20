de95d7adeb482d5b24b74fcc0609861c44328e80
Bulletdrone <Samboots123@gmail.com>
2019-05-20 12:27:20 +0200

    Last test Gyro 20-5-2019

--- Wonder Route/Assets/Scripts/CameraRotation.cs
+++ Wonder Route/Assets/Scripts/CameraRotation.cs
@@ -4,13 +4,48 @@ using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
 {
+
+    Quaternion initialRotation;
+    Quaternion gyroInitialRotation;
+    bool gyroEnabled;
+
     void Start()
     {
+        initialRotation = transform.rotation;
         Input.gyro.enabled = true;
+        gyroInitialRotation = Input.gyro.attitude;
     }
 
     void Update()
     {
-        transform.localRotation = Input.gyro.attitude;
+        if (gyroEnabled)
+        {
+#if !UNITY_EDITOR
+            Quaternion offsetRotation = ConvertRotation(Quaternion.Inverse(gyroInitialRotation) * Input.gyro.attitude);
+            transform.rotation = initialRotation * offsetRotation;
+#else
+            //for unity editor contorl
+            float speed = 2.0f;
+            transform.Rotate(Input.GetAxis("Mouse Y") * speed, Input.GetAxis("Mouse X") * speed, 0);
+#endif
+        }
+    }
+
+    public void AlignGyro()
+    {
+        gyroEnabled = false;
+        transform.rotation = Quaternion.identity;
+    }
+
+    public void StartGyro()
+    {
+        initialRotation = transform.rotation;
+        gyroInitialRotation = Input.gyro.attitude;
+        gyroEnabled = true;
+    }
+
+    private static Quaternion ConvertRotation(Quaternion q)
+    {
+        return new Quaternion(q.x, q.y, -q.z, -q.w);
     }
 }
