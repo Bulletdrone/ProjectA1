d227b92492d56ceebe4e93c0bae47dc817a56618
m.eind <36227686+4561351658456@users.noreply.github.com>
2019-05-23 12:01:47 +0200

    Cleaned up UI scene, added stuff to Arcore TestScenes etc

---
+++ Wonder Route/Assets/Scripts/ARController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using GoogleARCore;
+using GoogleARCore.Examples.Common;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ARController : MonoBehaviour
+{
+    public Camera FirstPersonCamera;
+    public GameObject DetectedPlanePrefab;
+    public GameObject LocationPrefab;
+    private bool m_IsQuitting = false;
+
+    void Update()
+    {
+        
+    }
+}
