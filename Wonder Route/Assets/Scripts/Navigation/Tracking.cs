65115e76539594f5354c6e004077d3d6a9e0131a
m.eind <36227686+4561351658456@users.noreply.github.com>
2019-06-03 12:25:19 +0200

    Add progress on worldmanager script

---
+++ Wonder Route/Assets/Scripts/Navigation/Tracking.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Android;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Tracking : MonoBehaviour
+{
+    [SerializeField]
+    private Location locationData;
+
+    public int maxDistance;
+
+    public Text _naviText;
+    public Text _statusText;
+
+    public void Tracker()
+    {
+        StartCoroutine(StartGPSTracker());
+        _statusText.text = "Currently tracking";
+    }
+
+    IEnumerator StartGPSTracker()
+    {
+        _naviText.text = "Tracking...";
+
+        yield return new WaitForSeconds(3);
+
+        if (!Input.location.isEnabledByUser)
+        {
+            _statusText.text = "Currently not tracking";
+            yield break;
+        }
+
+        Input.location.Start(maxDistance);
+
+        int waitUntilTimeOut = 20;
+
+        while (Input.location.status == LocationServiceStatus.Initializing && waitUntilTimeOut > 0)
+        {
+            yield return new WaitForSeconds(1);
+            _naviText.text = Input.location.status.ToString();
+            waitUntilTimeOut--;
+        }
+
+        if (waitUntilTimeOut < 1)
+        {
+            _naviText.text = "Timed Out";
+            yield break;
+        }
+
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            _naviText.text = "Failed to launch locationservice";
+            yield break;
+        }
+        if (Input.location.status == LocationServiceStatus.Running)
+        {
+            InvokeRepeating("TrackLocation", 1f, 1f);
+        }
+        else
+        {
+            _naviText.text = "Nothings happening";
+        }
+    }
+
+    void TrackLocation()
+    {
+        _naviText.text = "Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp;
+        if (Vector2.Distance(new Vector2(Input.location.lastData.latitude, Input.location.lastData.longitude), new Vector2(0,0/*Float latitude, float longitude*/)) < maxDistance)
+        {
+            //doStuff();
+        }
+    }
+}
---
+++ Wonder Route/Assets/Scripts/WorldManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldManager : MonoBehaviour
+{
+    public List<GameObject> worldScenes;
+    public GameObject spawnLocation;
+    Location locations;
+
+    void InstantiateWorld()
+    {
+        switch (locations)
+        {
+            case Location.SintLucasIngang:
+                //Instantiate(worldScenes[0], spawnLocation.positio);
+                break;
+            case Location.bagelnJuice:
+                break;
+            case Location.Sportcomplex:
+                break;
+            case Location.StationStrijp:
+                break;
+            case Location.End:
+                break;
+            default:
+                break;
+        }
+    }
+}
