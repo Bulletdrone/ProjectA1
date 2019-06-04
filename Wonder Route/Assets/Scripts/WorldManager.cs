9f5485e4001ff60f47334ee773342b2fb60d2dbd
m.eind <36227686+4561351658456@users.noreply.github.com>
2019-06-04 09:44:00 +0200

    Added setup of the worldmanager and testing prefabs

--- Wonder Route/Assets/Scripts/WorldManager.cs
+++ Wonder Route/Assets/Scripts/WorldManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class WorldManager : MonoBehaviour
 {
     public List<GameObject> worldScenes;
-    public GameObject spawnLocation;
+    public Transform spawnLocation;
     Location locations;
 
     void InstantiateWorld()
@@ -13,15 +13,19 @@ public class WorldManager : MonoBehaviour
         switch (locations)
         {
             case Location.SintLucasIngang:
-                //Instantiate(worldScenes[0], spawnLocation.positio);
+                Instantiate(worldScenes[0], spawnLocation.position, spawnLocation.rotation);
                 break;
             case Location.bagelnJuice:
+                Instantiate(worldScenes[1], spawnLocation.position, spawnLocation.rotation);
                 break;
             case Location.Sportcomplex:
+                Instantiate(worldScenes[3], spawnLocation.position, spawnLocation.rotation);
                 break;
             case Location.StationStrijp:
+                Instantiate(worldScenes[4], spawnLocation.position, spawnLocation.rotation);
                 break;
             case Location.End:
+                Instantiate(worldScenes[5], spawnLocation.position, spawnLocation.rotation);
                 break;
             default:
                 break;
